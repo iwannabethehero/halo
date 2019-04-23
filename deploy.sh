354ee32322808b919ac13a648995cad848b11d2b
hanlz <hanlz@cheche365.com>
2019-04-23 10:36:17 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # 指定Halo的根目录，请按实际修改
-HALO_DIR="/www/wwwroot/halo"
-
-# 拉取最新的源码
-# git pull
+HALO_DIR="/var/jenkins_home/workspace/halo-blog"
 
 # 进入Halo根目录
 cd $HALO_DIR
@@ -12,8 +9,6 @@ cd $HALO_DIR
 # 停止Halo
 sh $HALO_DIR/bin/halo.sh stop
 
-# 执行打包
-mvn package -Pprod
 
 # 进入打包好的Halo目录
 cd $HALO_DIR/target/dist/halo
